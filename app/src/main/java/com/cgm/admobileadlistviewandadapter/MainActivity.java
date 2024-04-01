a6b7e19dc7cea2801a2c86e2934850ad027fe0ad
CGM <mrmouse.guru@gmail.com>
2024-04-01 14:10:31 +0700

    put Country List to Custom layout ListView without Images

--- app/src/main/java/com/cgm/admobileadlistviewandadapter/MainActivity.java
+++ app/src/main/java/com/cgm/admobileadlistviewandadapter/MainActivity.java
@@ -24,9 +24,10 @@ public class MainActivity extends AppCompatActivity {
 
         ListView countryListView = findViewById(R.id.countryListView);
 
-        ArrayAdapter<Country> adapter = new ArrayAdapter<Country>(this,
-                android.R.layout.simple_list_item_1, list);
+        //ArrayAdapter<Country> adapter = new ArrayAdapter<Country>(this,
+                //android.R.layout.simple_list_item_1, list);
+        CustomListAdapter customListAdapter = new CustomListAdapter(list, this);
 
-        countryListView.setAdapter(adapter);
+        countryListView.setAdapter(customListAdapter);
     }
 }
